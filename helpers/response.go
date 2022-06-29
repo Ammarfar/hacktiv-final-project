87a98e0dd71c1fd1c04669c4997f6b41f87a6f35
Ammarfar <ammarfarghani@gmail.com>
2022-06-29 10:47:58 +0700

    add response helper

---
+++ helpers/response.go
@@ -0,0 +1,44 @@
+package helpers
+
+type Response interface {
+	SuccessWithData(string, interface{}) *response
+	Success(string) *response
+	Error(string) *response
+}
+
+type response struct {
+	Error   bool        `json:"error"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data"`
+}
+
+type responseImpl struct {
+}
+
+func NewResponse() Response {
+	return &responseImpl{}
+}
+
+func (r *responseImpl) SuccessWithData(message string, data interface{}) *response {
+	return &response{
+		Error:   false,
+		Message: message,
+		Data:    data,
+	}
+}
+
+func (r *responseImpl) Success(message string) *response {
+	return &response{
+		Error:   false,
+		Message: message,
+		Data:    nil,
+	}
+}
+
+func (r *responseImpl) Error(message string) *response {
+	return &response{
+		Error:   true,
+		Message: message,
+		Data:    nil,
+	}
+}
