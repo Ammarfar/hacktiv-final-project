d3c0dd76ac0fbeb77e6844d9f20d4078c64768a8
Ammar Farghani <ammar>
2022-07-02 01:26:25 +0700

    fix register response

--- controllers/user_controller.go
+++ controllers/user_controller.go
@@ -41,7 +41,12 @@ func (uc *userControllerImpl) Register(c *gin.Context) {
 		return
 	}
 
-	c.JSON(http.StatusOK, uc.response.SuccessWithData("User Created Successfully", user.Username))
+	c.JSON(http.StatusOK, uc.response.SuccessWithData("User Created Successfully", helpers.ResponseObj{
+		"age":     user.Age,
+		"email":   user.Email,
+		"id":      user.ID,
+		"usernam": user.Username,
+	}))
 }
 
 func (uc *userControllerImpl) Login(c *gin.Context) {
